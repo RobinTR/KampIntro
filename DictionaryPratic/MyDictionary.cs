a8b641fe5d189531714c130e5f64b099e41ab606
RobN <RobN@ROBN>
2022-06-23 19:47:34 +0300

    Gun4 + Odev

---
+++ DictionaryPratic/MyDictionary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryPratic
+{
+    public class MyDictionary<K, V>
+    {
+        K[] _key;
+        V[] _value;
+
+        public MyDictionary()
+        {
+            _key = new K[0];
+            _value = new V[0];
+        }
+
+        public void Add(K key, V value)
+        {
+            K[] _tempKey = _key;
+            V[] _tempValues = _value;
+            _key = new K[_key.Length + 1];
+            _value = new V[_key.Length + 1];
+            for (int i = 0; i < _tempKey.Length; i++)
+            {
+                _key[i] = _tempKey[i];
+                _value[i] = _tempValues[i];
+            }
+
+            _key[_key.Length - 1] = key;
+            _value[_key.Length - 1] = value;
+        }
+
+        public void Listele()
+        {
+            for (int i = 0; i < _key.Length; i++)
+            {
+                Console.WriteLine("[{0}, {1}]", _key[i], _value[i]);
+            }
+        }
+
+        public int Count
+        {
+            get { return _key.Length; }
+        }
+    }
+}
---
+++ DictionaryPratic/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+namespace DictionaryPratic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> ogrenci = new MyDictionary<int, string>();
+            ogrenci.Add(1,"Robin");
+            ogrenci.Add(2,"Test");
+            Console.WriteLine(ogrenci.Count);
+            ogrenci.Listele();
+
+        }
+    }
+
+}
