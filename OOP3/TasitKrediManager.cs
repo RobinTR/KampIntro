87577daa544bee7275724869de5fdea375fcc970
RobN <RobN@ROBN>
2022-06-24 13:28:14 +0300

    5. Gün

---
+++ OOP3/TasitKrediManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    class TasitKrediManager:IKrediManager
+    {
+        public void Hesapla()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BirseyYap()
+        {
+            //
+            Console.WriteLine("Taşıt kredisi ödeme planı hesaplandı.");
+        }
+    }
+}
